d063248803e57bfe1e25fecb3bd5bf2fbaf8ec05
Kilder <kilder.vsp@hotmail.com>
2021-08-06 11:26:02 -0300

    style: :lipstick: file imports

--- lib/widgets/buttons/buttons.dart
+++ lib/widgets/buttons/buttons.dart
@@ -1,6 +1,7 @@
 export 'button_side_bar.dart';
 export 'check_box.dart.dart';
 export 'filter.dart';
+export 'flat_button.dart';
 export 'radio_button.dart';
 export 'rouded_check_box.dart';
 export 'rounded_button.dart';
