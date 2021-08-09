747eaf7ed07259dad38defa96f83d8edbc7a33d5
Elyson Sarmento <elyson.sousa@Outlook.com>
2021-08-09 17:15:04 -0300

    Adicionando espaçamento e colocando o Main como Start.

--- lib/widgets/buttons/button_side_bar.dart
+++ lib/widgets/buttons/button_side_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lux_ui/lib.dart';
 
-
 class XSideBarButton extends StatelessWidget {
   final IconData icon;
   final String? text;
@@ -9,32 +8,44 @@ class XSideBarButton extends StatelessWidget {
   final double? fontSize;
   final Color? color;
   final double? iconSize;
-  
 
-  const XSideBarButton({ Key? key, required this.icon,  this.text = '',  this.onTap, this.fontSize = 30, this.color, this.iconSize }) : super(key: key);
+  const XSideBarButton(
+      {Key? key,
+      required this.icon,
+      this.text = '',
+      this.onTap,
+      this.fontSize = 28,
+      this.color,
+      this.iconSize = 30})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var xTheme = XTheme.of(context);
-    return GestureDetector(
-      onTap: onTap,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon,
-          size: iconSize,
-          color: color,
-          ),
-
-          SizedBox(width: 10,),
-
-          Text(text!,
-          style: TextStyle(
-            color: color ?? xTheme.backgroundColor,
-            fontSize: fontSize,
-            
-          ),)
-        ],
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 12),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              icon,
+              size: iconSize,
+              color: color ?? xTheme.backgroundColor,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              text!,
+              style: TextStyle(
+                  color: color ?? xTheme.backgroundColor,
+                  fontSize: fontSize,
+                  fontWeight: FontWeight.w300),
+            )
+          ],
+        ),
       ),
     );
   }
