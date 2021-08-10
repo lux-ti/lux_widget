690e77aee8c256004ab6c9c149257cbd2ef1c2d4
Kilder-M <kilder.vsp@hotmail.com>
2021-08-10 10:11:26 -0300

    Atualizando design

--- lib/widgets/status.dart
+++ lib/widgets/status.dart
@@ -4,21 +4,37 @@ import 'package:lux_ui/lib.dart';
 class XStatus extends StatelessWidget {
   final String? name;
   final Color? color;
+  final int? intDays;
 
-  const XStatus({ Key? key, this.name, this.color }) : super(key: key);
+  const XStatus({ Key? key, this.name, this.color = Colors.red, this.intDays }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var xTheme = XTheme.of(context);
 
-    return Text(
-      '',
-      style: TextStyle(
-        color: color ?? xTheme.dangeColor,
-        fontSize: 12,
-        fontWeight: FontWeight.w800,
-        
-      ),
+    return Row(
+      children: [
+        Container(
+          height: 5,
+          width: 5,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(15)
+          ),
+        ),
+
+        SizedBox(width: 5,),
+
+        Text(
+          '$name' + ' $intDays' + ' DIAS',
+          style: TextStyle(
+            color: color ?? xTheme.dangeColor,
+            fontSize: 12,
+            fontWeight: FontWeight.w800,
+            
+          ),
+        ),
+      ],
     );
   }
 }
