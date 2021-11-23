e4ff4b1dbf0be10e86c9c61e65de69fef7ae8bda
Kilder Montinegro <kilder.vsp@hotmail.com>
2021-11-23 12:09:46 -0300

    adicionando parametros nomeados e alterando design

--- lib/widgets/Scafold/scafold_grey.dart
+++ lib/widgets/Scafold/scafold_grey.dart
@@ -12,10 +12,24 @@ class XScafoldGrey extends XScafold {
   final String? codeNumber;
   final String? nameStatus;
   final IconData? iconStatus;
+  final Color? iconVerifiedBackgroundColor;
+  final Color? roundedIconBackGroundColor;
+  final Color? iconVerifiedColor;
+  final Color? roundedIconColor;
+  final bool? isStatus;
+  final bool? isRoundedIconbutton;
+  final bool? isIconVerified;
   final void Function()? onPressed;
 
   XScafoldGrey({
+    this.iconVerifiedColor,
+    this.roundedIconColor,
+    this.iconVerifiedBackgroundColor,
+    this.roundedIconBackGroundColor,
+    this.isIconVerified,
     this.iconStatus,
+    this.isStatus,
+    this.isRoundedIconbutton,
     this.nameStatus,
     this.colorStatus,
     this.onPressed,
@@ -32,6 +46,7 @@ class XScafoldGrey extends XScafold {
   Widget buildTop(context) {
     final parentRoute = ModalRoute.of(context);
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         if (onBack != null || parentRoute?.canPop == true) ...[
           buildBackButton(context),
@@ -39,54 +54,78 @@ class XScafoldGrey extends XScafold {
         SizedBox(
           width: 8,
         ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            (codeNumber != null) ? codeNumberText() : Container(),
-            Padding(
-              padding: EdgeInsets.only(bottom: codeNumber != null ? 20 : 0),
-              child: buildTitle(),
-            ),
-          ],
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        (onPressed != null)
-            ? RoundedIconButton(
-                onPressed: onPressed,
-                width: 35,
-                height: 35,
-                backgroundColor: theme.primaryColor,
-                iconColor: theme.backgroundColor,
-                iconSize: 20,
-                icon: Icons.add,
-              )
-            : Container(),
-        SizedBox(
-          width: 20,
-        ),
-        (nameStatus != null)
-            ? Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        Expanded(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                (codeNumber != null) ? codeNumberText() : Container(),
+                Row(
                   children: [
-                    XStatusBox(
-                      name: nameStatus!,
-                      color: colorStatus ?? Colors.transparent,
+                    buildTitle(),
+                    SizedBox(
+                      width: 2,
                     ),
-                    (iconStatus != null)
-                        ? RoundedIconButton(
-                            icon: iconStatus!,
-                            backgroundColor: colorStatus ?? Colors.transparent,
-                            iconColor: Colors.white,
-                          )
-                        : Container()
+                    Container(
+                      height: 30,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          (isRoundedIconbutton == true)
+                              ? RoundedIconButton(
+                                  onPressed: onPressed,
+                                  width: 21,
+                                  height: 21,
+                                  backgroundColor: roundedIconBackGroundColor ??
+                                      theme.primaryColor,
+                                  iconColor:
+                                      roundedIconColor ?? theme.backgroundColor,
+                                  iconSize: 21,
+                                  icon: Icons.add,
+                                )
+                              : Container(),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            (isStatus == true)
+                                ? XStatusBox(
+                                    name: nameStatus ?? '',
+                                    color: colorStatus ?? theme.primaryColor,
+                                  )
+                                : Container(),
+                            (isIconVerified == true)
+                                ? RoundedIconButton(
+                                    icon: Icons.check,
+                                    backgroundColor:
+                                        iconVerifiedBackgroundColor ??
+                                            theme.primaryColor,
+                                    iconColor: iconVerifiedColor ??
+                                        theme.backgroundColor,
+                                    width: 31,
+                                    height: 31,
+                                    iconSize: 31,
+                                  )
+                                : Container()
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
-              )
-            : Container(),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
@@ -108,7 +147,8 @@ class XScafoldGrey extends XScafold {
         style: TextStyle(
             color: theme.backgroundColor,
             fontSize: 12,
-            fontWeight: FontWeight.w700,
+            fontWeight: FontWeight.w500,
+            fontFamily: 'Manrope',
             letterSpacing: 1));
   }
 
