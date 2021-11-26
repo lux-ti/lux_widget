1e670f958cdcfe503c4a74c621b9b2d860fcf8c8
arielsardinha <odararepresentacoes@outlook.com.br>
2021-11-26 10:12:37 -0200

    feat(XFilter): colocando search pra funcionar

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:example/views/XScafoldGrey.dart';
-import 'package:example/views/Xstatus.dart';
+import 'package:example/views/XFilter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:lux_ui/lux_ui.dart';
 
 main(List<String> args) {
   runApp(XApp(
     title: 'Exemplo',
-    home: XscaffoldTeste(),
+    home: XFilterTeste(),
   ));
 }
