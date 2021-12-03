63f2094e8e5a7cae10bcde7d0cdbaf81f1bd59cb
arielsardinha <odararepresentacoes@outlook.com.br>
2021-12-03 16:28:12 -0200

    feat(XDialog): modal com suporte para dialog e alert criada

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:example/views/XFilter.dart';
 import 'package:example/views/XLogin.dart';
+import 'package:example/views/dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:lux_ui/lux_ui.dart';
 
 main(List<String> args) {
   runApp(XApp(
     title: 'Exemplo',
-    home: LoginView(),
+    home: Xdialog(),
   ));
 }
