3a09cac676b8298d995b88d6a224b484b0f43329
arielsardinha <odararepresentacoes@outlook.com.br>
2021-12-13 10:01:46 -0200

    feat(XDropdownSearch): colocando evento de onDelete

--- lib/widgets/x-dropdown-search/x-dropdown-search.dart
+++ lib/widgets/x-dropdown-search/x-dropdown-search.dart
@@ -5,6 +5,7 @@ class XDropdownSearch extends StatefulWidget {
   final void Function(String, bool)? onChanged;
   final void Function(String)? onTapItem;
   final void Function()? infinity;
+  final void Function(String?)? onDelete;
   final TextEditingController controller;
   final String? placeholder;
   final Color? placeholderColor;
@@ -24,6 +25,7 @@ class XDropdownSearch extends StatefulWidget {
     required this.items,
     this.width,
     this.onTapItem,
+    this.onDelete,
   }) : super(key: key);
 
   @override
@@ -135,6 +137,9 @@ class _XDropdownSearchState extends State<XDropdownSearch> {
                     onTap: () {
                       widget.controller.text = '';
                       FocusScopeNode currentFocus = FocusScope.of(context);
+                      if (widget.onDelete != null) {
+                        widget.onDelete!(widget.controller.text);
+                      }
                       if (!currentFocus.hasPrimaryFocus) {
                         currentFocus.unfocus();
                       }
