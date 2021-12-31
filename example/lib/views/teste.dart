62f63c50ac49b877cf74bf4de6202e361b6b50d6
arielsardinha <odararepresentacoes@outlook.com.br>
2021-12-31 12:23:41 -0200

    fix(DropdownSearchItem): corrigindo design

--- example/lib/views/teste.dart
+++ example/lib/views/teste.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:lux_ui/widgets/Scafold/Scafold.dart';
 import 'package:lux_ui/widgets/x-dropdown-search/x-dropdown-search.dart';
 
@@ -18,12 +19,43 @@ List<DropdownSearchItem> teste = [
   DropdownSearchItem(text: 'teste5', value: 5, id: 5),
   DropdownSearchItem(text: 'teste6', value: 6, id: 6),
   DropdownSearchItem(text: 'teste7', value: 7, id: 7),
+  DropdownSearchItem(text: 'teste8', value: 8, id: 8),
+  DropdownSearchItem(text: 'teste', value: 0, id: 0),
+  DropdownSearchItem(text: 'teste1', value: 1, id: 1),
+  DropdownSearchItem(text: 'teste2', value: 2, id: 2),
+  DropdownSearchItem(text: 'teste3', value: 3, id: 3),
+  DropdownSearchItem(text: 'teste4', value: 4, id: 4),
+  DropdownSearchItem(text: 'teste5', value: 5, id: 5),
+  DropdownSearchItem(text: 'teste6', value: 6, id: 6),
+  DropdownSearchItem(text: 'teste7', value: 7, id: 7),
+  DropdownSearchItem(text: 'teste8', value: 8, id: 8),
+  DropdownSearchItem(text: 'teste', value: 0, id: 0),
+  DropdownSearchItem(text: 'teste1', value: 1, id: 1),
+  DropdownSearchItem(text: 'teste2', value: 2, id: 2),
+  DropdownSearchItem(text: 'teste3', value: 3, id: 3),
+  DropdownSearchItem(text: 'teste4', value: 4, id: 4),
+  DropdownSearchItem(text: 'teste5', value: 5, id: 5),
+  DropdownSearchItem(text: 'teste6', value: 6, id: 6),
+  DropdownSearchItem(text: 'teste7', value: 7, id: 7),
   DropdownSearchItem(text: 'teste8', value: 8, id: 8)
+];
+
+List<DropdownSearchItem> teste2 = [
+  DropdownSearchItem(text: 'teste', value: 0, id: 0),
+  DropdownSearchItem(text: 'teste1', value: 1, id: 1),
+  DropdownSearchItem(text: 'teste2', value: 2, id: 2),
+  DropdownSearchItem(text: 'teste3', value: 3, id: 3),
+  DropdownSearchItem(text: 'teste4', value: 4, id: 4),
+  DropdownSearchItem(text: 'teste5', value: 5, id: 5),
+  DropdownSearchItem(text: 'teste6', value: 6, id: 6),
+  DropdownSearchItem(text: 'teste7', value: 7, id: 7),
+  DropdownSearchItem(text: 'teste8', value: 8, id: 8),
 ];
 
 int n = 1;
 
-var controllert = TextEditingController();
+TextEditingController teste1controller = TextEditingController();
+TextEditingController teste2controller = TextEditingController();
 
 class _TesteState extends State<Teste> {
   @override
@@ -45,18 +77,40 @@ class _TesteState extends State<Teste> {
       title: "teste ",
       firstOnPressed: () {},
       onBack: () {},
-      child: XDropdownSearch(
-        controller: controllert,
-        onTapItem: (value) {
-          print(value.text);
-        },
-        onDelete: (value) {
-          print("value $value");
-          print(controllert.text);
-        },
-        onChanged: (value, has) {},
-        items: teste,
-        infinity: add,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            XDropdownSearch(
+              controller: teste1controller,
+              onTapItem: (value) {
+                print("CONTROLLER ${teste1controller.text}");
+              },
+              onChanged: (value, has) {},
+              items: teste,
+              infinity: add,
+            ),
+            XDropdownSearch(
+              controller: teste2controller,
+              onTapItem: (value) {
+                print(value.text);
+              },
+              onChanged: (value, has) {},
+              items: teste2,
+              infinity: add,
+            ),
+            XDropdownSearch(
+              controller: teste1controller,
+              hintText: "Teste",
+              onTapItem: (value) {
+                print(value.text);
+              },
+              onChanged: (value, has) {},
+              items: teste,
+              infinity: add,
+            ),
+          ],
+        ),
       ),
     );
   }
